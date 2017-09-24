380ef577b449669266804d6a42816849cf7fe4fd
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2017-09-24 13:46:53 +0530

    Update Average of two numbers.java

--- Source Codes/Average of two numbers.java
+++ Source Codes/Average of two numbers.java
@@ -4,16 +4,13 @@ public class average
 		public static void main(String[] args)
 			{   
 				Scanner in=new Scanner (System.in);
-				int a, b, avg;
-				System.out.print ("\n\t Enter first number = ");
-				a = in.nextInt();
-				System.out.print ("\n\t Enter second number = ");
-				b = in.nextInt();
+				double a, b, avg;
+				System.out.print ("\n\t Enter the first number: ");
+				a = in.nextDouble();
+				System.out.print ("\n\t Enter the second number: ");
+				b = in.nextDouble();
 				a += b;
-				avg = (a)/2;
-				if (avg>50)
-					System.out.println ("\n\t Error ");
-				else 
-					System.out.println ("\n\t Sum = "+a+"    Average = "+avg);
+				avg = (a)/2.0;
+				System.out.println ("\n\t Sum = "+a+"    Average = "+avg);
 	}
 			}
