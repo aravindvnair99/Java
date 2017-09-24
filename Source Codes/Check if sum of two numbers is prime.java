7a2a1e888fc06ec3f5d795970da10897d9805ac1
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2017-09-24 20:17:04 +0530

    Update Check if sum of two numbers is prime.java

--- Source Codes/Check if sum of two numbers is prime.java
+++ Source Codes/Check if sum of two numbers is prime.java
@@ -3,23 +3,23 @@ public class prime
     {
         public static void main (String arg[])
             {
-                Scanner in=new Scanner (System.in);
-                int a, b=2, c=1, d;
-                System.out.print ("\n\t Enter first number = ");
-                a = in.nextInt();
-                System.out.print ("\n\t Enter second number = ");
-                d = in.nextInt();
-                a += d;
-                System.out.println ("\n\t Sum of the numbers = "+a);
+                Scanner in=new Scanner (System.in); //Preparing input function
+                double a, b=2, c=1, d; //Declaring variables and assigning values
+                System.out.print ("\n\t Enter first number: "); //Asking user for value of a
+                a = in.nextDouble(); //Storing first number as 'a' of data type 'double'
+                System.out.print ("\n\t Enter second number: "); //Asking user for value of d
+                d = in.nextDouble(); //Storing second number as 'b' of data type 'double'
+                a += d; //a = a + d (Sum)
+                System.out.println ("\n\t Sum of the numbers = "+a); //Output sum
                 while (b <= a/2)
                     {
-                        if (a%b==0)
+                        if (a%b==0) //Check for modulus equal to 0
                             c = 0;
                             ++b;
                     }
-                if (c==0)
-                    System.out.println ("\n\t Sum of the numbers is NOT a prime number.");
+                if (c==0) //Check for prime
+                    System.out.println ("\n\t Sum of the numbers is NOT a prime number."); //Output not prime
                 else 
-                    System.out.println ("\n\t Sum of the numbers is a Prime number.");
+                    System.out.println ("\n\t Sum of the numbers is a Prime number."); //Output prime
             }
     }
