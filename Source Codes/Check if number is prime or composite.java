c67cc0fac3718c19d260fdcf5d2f979d6877d635
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2017-10-04 19:56:45 +0530

    Update Check if number is prime or composite.java

--- Source Codes/Check if number is prime or composite.java
+++ Source Codes/Check if number is prime or composite.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+public class prime
+    {
+        public static void main (String arg[])
+            {
+                Scanner in=new Scanner (System.in); //Preparing input function
+                double a, b=2, c=1, d; //Declaring variables and assigning values
+                System.out.print ("\n\t Enter the number: "); //Asking user for the number
+                a = in.nextDouble(); //Storing number as 'a' of data type 'double'
+                if (a==0 || a==1)
+                    System.out.println ("\n\t Neither prime nor composite."); //For 0 and 1
+                else
+                    {
+                        while (b <= a/2)
+                        {
+                            if (a%b==0) //Check for modulus equal to 0
+                            c = 0;
+                            ++b;
+                        }
+                        if (c==0) //Check for prime
+                            System.out.println ("\n\t Is a composite number."); //Output not prime
+                        else 
+                            System.out.println ("\n\t Is a Prime number."); //Output prime
+                    }
+            }
+    }
