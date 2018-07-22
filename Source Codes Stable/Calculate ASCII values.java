0e421a546913536ba3a28ead95723b4a8da6d381
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-07-22 14:53:53 +0530

    Refactor

--- Source Codes Stable/Calculate ASCII values.java
+++ Source Codes Stable/Calculate ASCII values.java
@@ -1,15 +1,14 @@
-import java.util.*;
-public class ascii
-    {
-        public static void main (String arg[])
-        throws java.io.IOException
-            {
-                int b; //Declaration for storing ASCII number
-                char a; //Declaration for storing user input
-                Scanner in=new Scanner (System.in); //Preparing input function
-                System.out.print ("\n\t Enter a character = "); //Asking user for value of a
-                a=(char) System.in.read(); //Live read value entered by user
-                b=(int)a; //Convert from char to int data type
-                System.out.println ("\n\t The ASCII value of "+a+" is = "+b); //Output ASCII value
-            }
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String arg[])
+    throws java.io.IOException {
+        int b;
+        char a;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a character:");
+        a = (char) System.in.read();
+        b = (int) a;
+        System.out.println("The ASCII value of " + a + " is = " + b);
     }
+}
--- Source Codes Stable/Check if number is prime or composite.java
+++ Source Codes Stable/Check if number is prime or composite.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
-public class prime
-    {
-        public static void main (String arg[])
-            {
-                Scanner in=new Scanner (System.in); //Preparing input function
-                double a, b=2, c=1; //Declaring variables and assigning values
-                System.out.print ("\n\t Enter the number: "); //Asking user for the number
-                a = in.nextDouble(); //Storing number as 'a' of data type 'double'
-                if (a==0 || a==1)
-                    System.out.println ("\n\t Neither prime nor composite."); //For 0 and 1
-                else
-                    {
-                        while (b <= a/2)
-                            {
-                                if (a%b==0) //Check for modulus equal to 0
-                                    c = 0;
-                                ++b;
-                            }
-                        if (c==0) //Check for prime
-                            System.out.println ("\n\t Is a composite number."); //Output not prime
-                        else 
-                            System.out.println ("\n\t Is a Prime number."); //Output prime
-                    }
+
+public class prime {
+    public static void main (String arg[]) {
+        Scanner in = new Scanner (System.in);
+        double a, b = 2, c = 1;
+        System.out.println("Enter the number:");
+        a = in.nextDouble();
+        if (a==0 || a==1)
+            System.out.println("Neither prime nor composite.");
+        else {
+            while (b <= a/2) {
+                if (a%b==0)
+                    c = 0;
+                ++b;
             }
+            if (c==0)
+                System.out.println("Is a composite number.");
+            else 
+                System.out.println("Is a Prime number.");
+        }
     }
+}
