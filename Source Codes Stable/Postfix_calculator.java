e76380f888ee257a158ec0cba0e715b9c50746bc
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-08-01 21:13:57 +0530

    Postfix

--- Source Codes Stable/Postfix_calculator.java
+++ Source Codes Stable/Postfix_calculator.java
@@ -2,19 +2,17 @@ import java.util.Stack;
  
 public class Postfix_calculator {
     static int evaluatePostfix(String exp) {
-        //create a stack using built in stack
+        //Create a stack using built in stack
         Stack<Integer> stack=new Stack<>();
         /*create a stack using your own stack
         StackX stack = new StackX(10);*/
-        // Scan all characters one by one
+        //Scan all characters one by one
         for(int i=0;i<exp.length();i++) {
             char c=exp.charAt(i);
-            // If the scanned character is an operand (number here),
-            // push it to the stack.
+            //If the scanned character is an operand (number here), push it to the stack.
             if(Character.isDigit(c))
                stack.push(c - '0');
-            //  If the scanned character is an operator, pop two
-            // elements from stack apply the operator
+            //If the scanned character is an operator, pop two elements from stack apply the operator
             else {
                 int val1 = stack.pop();
                 int val2 = stack.pop();
