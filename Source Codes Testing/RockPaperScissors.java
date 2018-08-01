1806799ad763084b0f0ba07586c183d5ff6ee3d3
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-08-01 19:47:32 +0530

    Add

---
+++ Source Codes Testing/RockPaperScissors.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RockPaperScissors {
+	public static void main(String[] args) {
+		System.out.println("Enter 1 for rock.");
+		System.out.println("Enter 2 for paper.");
+		System.out.println("Enter 3 for scissor.");
+		System.out.println("Enter 4 for score.");
+		System.out.println("Press any other key to exit. \n");
+		Scanner sc = new Scanner(System.in);
+		int player = sc.nextInt();
+		int comp = ThreadLocalRandom.current().nextInt(1,4);
+		int comp_point = 0, player_point = 0;
+		if (player == 1 || player == 2 || player == 3) {
+			if ((comp == 1 && player == 2) || (comp == 2 && player == 1) || (comp == 3 && player == 2)) {
+				comp_point++;
+				System.out.print("\nComputer wins with ");
+				if (comp == 1)
+					System.out.print("rock.");
+				else if (comp == 2)
+					System.out.print("paper.");
+				else
+					System.out.print("scissor.");
+			}
+			else {
+				player_point++;
+				System.out.print("\nYou win as computer put ");
+				if (comp == 1)
+					System.out.print("rock.");
+				else if (comp == 2)
+					System.out.print("paper.");
+				else
+					System.out.print("scissor.");
+			}
+		}
+		else {
+			sc.close();
+			return;
+		}
+		sc.close();
+	}
+}
---
+++ Source Codes Testing/StackApp.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+class StackX
+{
+private int maxSize;
+private long[] stackArray;
+private int top;  // points the current top of the stack
+public StackX(int s)
+{
+maxSize = s;
+stackArray = new long[maxSize]; 
+top = -1;
+}
+
+public void push(long j)
+
+{
+if(isFull())
+{
+  System.out.println("Stack is full");
+}
+else
+stackArray[++top] = j;
+
+}
+
+public long pop()
+
+{
+if(isEmpty()){
+System.out.println("Stack is Empty"); 
+return -99;
+}
+else
+return stackArray[top--]; 
+}
+
+public long peek()
+
+{
+return stackArray[top];
+}
+
+public boolean isEmpty()
+
+{
+return (top == -1);
+}
+
+public boolean isFull()
+
+{
+return (top == maxSize-1);
+}
+
+} 
+
+public class StackApp
+{
+public static void main(String[] args)
+{
+StackX theStack = new StackX(5); // creating an object of the class StackX
+
+Scanner scan = new Scanner(System.in);
+
+		int opt = 0;
+		do {
+			System.out.println("1.Push");
+			System.out.println("2.Pop");
+			System.out.println("3.Size");
+			System.out.println("4.Top");
+			System.out.println("5.PrintStack");
+			System.out.println("6.Exit");
+			opt = scan.nextInt();
+			switch (opt) {
+			case 1:
+				System.out.println("Enter value to push");
+				int data = scan.nextInt();
+			theStack.push(data);
+				break;
+			case 2:
+                                         long p;
+                                         p=theStack.pop();
+                                         if(p==-99){}
+                                         else				
+                                           System.out.println("Popped value is " + p);
+				break;
+		/*	case 3:
+				System.out.println("Size is " + theStack.size());
+				break;*/
+			case 4:
+				System.out.println("Top is " + theStack.peek());
+				break;
+			/*case 5:
+				theStack.printStack();
+				break;*/
+			case 6:
+				break;
+			default:
+				System.out.println("Enter Again");
+				break;
+			}
+		} while (opt != 6);
+} 
+}
+
