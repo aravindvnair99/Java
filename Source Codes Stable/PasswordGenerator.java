4798f9a823dd94facf9a6a5e699b65054194d886
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-09-13 17:05:31 +0530

    Create PasswordGenerator.java

---
+++ Source Codes Stable/PasswordGenerator.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the password you require:\n");
+        System.out.println(generatePswd(sc.nextInt()));
+        sc.close();
+    }
+    static char[] generatePswd(int len) {
+        System.out.println("\nYour password is:\n");
+        String charsCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String chars = "abcdefghijklmnopqrstuvwxyz";
+        String nums = "0123456789";
+        String symbols = "!@#$%^&*_=+-/€.?<>)";
+        String passSymbols = charsCaps + chars + nums + symbols;
+        Random rnd = new Random();
+        char[] password = new char[len];
+        int index = 0;
+        for (int i = 0; i < len; i++) {
+            password[i] = passSymbols.charAt(rnd.nextInt(passSymbols.length()));
+        }
+        return password;
+    }
+}
