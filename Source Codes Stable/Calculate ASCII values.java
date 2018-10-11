2dcb39668224ad1c07c1b881c7b95a6fe86c2c24
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-10-11 10:07:21 +0530

    Refactor

--- Source Codes Stable/Calculate ASCII values.java
+++ Source Codes Stable/Calculate ASCII values.java
@@ -1,16 +1,12 @@
 // Calculate ASCII value of character
 
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String arg[]) throws java.io.IOException {
 		int b;
 		char a;
-		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a character:");
 		a = (char) System.in.read();
 		b = (int) a;
 		System.out.println("The ASCII value of " + a + " is = " + b);
-		in.close();
 	}
 }
