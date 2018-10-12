24bff3a5cbe36a4136272ad6493b2474451717b6
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-10-12 18:01:51 +0530

    Update Multiplication.java

--- Source Codes Stable/Multiplication.java
+++ Source Codes Stable/Multiplication.java
@@ -1,16 +1,15 @@
-// Addition of two numbers from input
+// Multiplication of two numbers from input
 
 import java.util.Scanner;
 
 public class Multiplication {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter 2 numbers:\n");
-		double n1, n2, mult;
+		System.out.print("Enter 2 numbers:\n");
+		double n1, n2;
 		n1 = sc.nextDouble();
 		n2 = sc.nextDouble();
-		mult = n1 * n2;
-		System.out.println("\nMultiplication = " + mult);
+		System.out.println("\nMultiplication = " + (n1*n2));
 		sc.close();
 	}
 }
