4d81798a263c3e1c9d94638778a70de55f696bff
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-11-01 08:55:46 +0530

    Update and rename IntoHours.java to SecondsToHours.java

--- Source Codes Stable/SecondsToHours.java
+++ Source Codes Stable/SecondsToHours.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class IntoHours {
+public class SecondsToHours {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int sec, min, hr;
-		System.out.println("Enter the time in secons");
+		System.out.println("Enter the time in seconds");
 		sec = sc.nextInt();
 		hr = sec / 3600;
 		sec = sec % 3600;
 		min = sec / 60;
 		sec = sec % 60;
-		System.out.println(hr + ":" + min + ":" + sec);
+		System.out.println("The time is " + hr + ":" + min + ":" + sec);
+		sc.close();
 	}
-
 }
