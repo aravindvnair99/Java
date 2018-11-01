d82e8b64ac6e4c9ca2429d18545aafa26385aba4
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-11-01 09:58:52 +0530

    Update and rename Prac.java to DistanceFromOrigin.java

--- Source Codes Stable/DistanceFromOrigin.java
+++ Source Codes Stable/DistanceFromOrigin.java
@@ -10,21 +10,21 @@ class point {
 	}
 
 	void set() {
-		x = 1;
-		y = 1;
+		x = 0;
+		y = 0;
 	}
 }
 
-public class Prac {
+public class DistanceFromOrigin {
 	public static void main(String[] args) {
 		point p1 = new point();
 		point p2 = new point();
 		p1.set();
-		Scanner as = new Scanner(System.in);
-		System.out.println("enter the co ordinates of second point");
-		p2.x = as.nextDouble();
-		p2.y = as.nextDouble();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the co ordinates of second point");
+		p2.x = sc.nextDouble();
+		p2.y = sc.nextDouble();
 		p1.dis(p2.x, p2.y);
-		as.close();
+		sc.close();
 	}
 }
